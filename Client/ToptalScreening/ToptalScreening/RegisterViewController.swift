9bca715ae469087f6c3d51ef24c5311401cc1156
Shbli <shbli@peoplecorpgaming.com>
2018-03-02 21:33:59 -0800

    Create the xCode project for iOS app Prepare some UIs

---
+++ Client/ToptalScreening/ToptalScreening/RegisterViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RegisterViewController.swift
+//  ToptalScreening
+//
+//  Created by Shbli on 2018-03-02.
+//  Copyright © 2018 Shbli. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func OnDoneClicked(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
