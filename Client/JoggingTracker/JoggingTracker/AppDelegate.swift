ace8fac34fe93b351817cb092fee07b10b17d930
Shbli <info@shbli.com>
2018-03-27 15:50:21 -0700

    Adding CocoaPods Add Branch.io pod Integration and test of deep linking preparing for a integration with commercial project
    Signed-off-by: Shbli <info@shbli.com>

--- Client/JoggingTracker/JoggingTracker/AppDelegate.swift
+++ Client/JoggingTracker/JoggingTracker/AppDelegate.swift
@@ -7,16 +7,43 @@
 //
 
 import UIKit
+import Branch
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // for debug and development only
+        Branch.getInstance().setDebug()
+        // listener for Branch Deep Link data
+        Branch.getInstance().initSession(launchOptions: launchOptions) { (params, error) in
+            // do stuff with deep link data (nav to page, display content, etc)
+            print("params as? [String: AnyObject] ?? {}")
+            print(params as? [String: AnyObject] ?? {})
+            print("Referring link")
+            print(params!["~referring_link"])
+        }
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        // handler for URI Schemes (depreciated in iOS 9.2+, but still used by some apps)
+        Branch.getInstance().application(app, open: url, options: options)
+        return true
+    }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
+        // handler for Universal Links
+        Branch.getInstance().continue(userActivity)
+        return true
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        // handler for Push Notifications
+        Branch.getInstance().handlePushNotification(userInfo)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -43,4 +70,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
---
+++ Client/JoggingTracker/Pods/Target Support Files/Pods-JoggingTrackerTests/Pods-JoggingTrackerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JoggingTrackerTests : NSObject
+@end
+@implementation PodsDummy_Pods_JoggingTrackerTests
+@end
